50d7a5d0d61946a64a6cf12c6ac33f61900c67c3
thomas <thomasmaikon@hotmail.com>
2023-04-22 12:27:28 -0300

    Feat: Exibindo resultado, os itens que sao mostrados

--- src/components/ExibirArvore.jsx
+++ src/components/ExibirArvore.jsx
@@ -7,22 +7,32 @@ import 'react-tree-graph/dist/style.css';
 export default function ExibirArvore(props) {
     const { itens, mochila, exibeResultado } = useContext(MyContext);
 
-    const arvore = itens.length > 0 
-        ? exibeResultado 
-            ? new No().gerarArvore(itens, mochila).converterEmArvore() 
-            : {} 
-        : {} ;
 
-    const data = {
-        name: 'Parent',
-        children: [{
-            name: 'Child One'
-        }, {
-            name: 'Child Two'
-        }]
-    };
+
+    const resultado = itens.length > 0
+        ? exibeResultado
+            ? new No().gerarArvore(itens, mochila)
+            : {}
+        : {};
+
+    const arvore = itens.length > 0
+        ? exibeResultado
+            ? resultado.converterEmArvore()
+            : {}
+        : {};
+
+    function exibirResultadoEsperado() {
+        if (resultado.resultadoEsperado != undefined) {
+            return resultado.resultadoEsperado.map((item) => {
+                return ` [${item.nome}] `
+            })
+        }
+    }
+
     return (
-        <>
+        <div>
+            {exibeResultado ? <div> Resultado: <span> { exibirResultadoEsperado() } </span> </div> : <></> }
+
             <Tree
                 data={arvore}
                 height={1000}
@@ -30,8 +40,8 @@ export default function ExibirArvore(props) {
                 svgProps={{
                     strokeWidth: 0.1,
                 }}
-                />
-        </>
+            />
+        </div>
 
     );
 
